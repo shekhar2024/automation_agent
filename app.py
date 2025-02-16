69c3a5f321ea2fb6acd7adae310b22bcdfac13d0
shekhar2024 <shekhartripathi38@gmail.com>
2025-02-16 23:15:00 +0530

    changed name

--- app.py
+++ app.py
@@ -19,8 +19,8 @@
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import PlainTextResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from taskA import *
-from taskB import *
+from tasksA import *
+from tasksB import *
 import requests
 from dotenv import load_dotenv
 import os
